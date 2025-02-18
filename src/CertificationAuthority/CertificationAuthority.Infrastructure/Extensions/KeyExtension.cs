8f0dc1231b97bf7abc552e754464b458e105db6c
Yuri Melo <11520745+elyosemite@users.noreply.github.com>
2025-02-18 00:06:38 -0300

    remove useless methods

--- src/CertificationAuthority/CertificationAuthority.Infrastructure/Extensions/KeyExtension.cs
+++ src/CertificationAuthority/CertificationAuthority.Infrastructure/Extensions/KeyExtension.cs
@@ -3,7 +3,6 @@ using CertificationAuthority.Domain.ValueObjects;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.OpenSsl;
-using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
 
 namespace CertificationAuthority.Infrastructure.Extensions;
@@ -12,12 +11,10 @@ public static class KeyExtension
 {
     public static PublicKey ToPublicKey(this byte[] der) => new PublicKey(der);
     public static PublicKey ToPublicKeyFromBase64(this string base64) => new PublicKey(Convert.FromBase64String(base64));
-    //public static PrivateKey ToPrivateKey(this byte[] der) => new PrivateKey(der);
-    //public static PrivateKey ToPrivateKeyFromBase64(this string base64) => new PrivateKey(Convert.FromBase64String(base64));
+    public static PrivateKey ToPrivateKey(this byte[] der) => new PrivateKey(der);
+    public static PrivateKey ToPrivateKeyFromBase64(this string base64) => new PrivateKey(Convert.FromBase64String(base64));
     //public static SymmetricKey ToSymmetricKey(this byte[] der) => new SymmetricKey(der);
     //public static SymmetricKey ToSymmetricKeyFromBase64(this string base64) => new SymmetricKey(Convert.FromBase64String(base64));
-
-
 
     public static AsymmetricCipherKeyPair Generate()
     {
@@ -78,54 +75,6 @@ public static class KeyExtension
                 return keyParameter;
 
             throw new InvalidOperationException("Invalid formato para Private Key - PEM.");
-        }
-    }
-
-    // Novos métodos
-    public static byte[] ToDer(this AsymmetricKeyParameter key)
-    {
-        return PrivateKeyInfoFactory.CreatePrivateKeyInfo(key).ToAsn1Object().GetDerEncoded();
-    }
-
-    public static string ToPem(this AsymmetricKeyParameter key, string header)
-    {
-        using (var sw = new StringWriter())
-        using (var pemWriter = new PemWriter(sw))
-        {
-            pemWriter.WriteObject(key);
-            return sw.ToString();
-        }
-    }
-
-    public static byte[] ToDer(this string pemString)
-    {
-        using (var reader = new StringReader(pemString))
-        {
-            var pemReader = new PemReader(reader);
-            var keyObject = pemReader.ReadObject();
-
-            if (keyObject is AsymmetricKeyParameter keyParameter)
-                return keyParameter.ToDer();
-            else if (keyObject is AsymmetricCipherKeyPair keyPair)
-                return keyPair.Private.ToDer();
-
-            throw new InvalidOperationException("Invalid key format.");
-        }
-    }
-
-    public static AsymmetricKeyParameter ToAsymmetricKeyParameter(this byte[] derData)
-    {
-        using (var reader = new StringReader(Convert.ToBase64String(derData)))
-        {
-            var pemReader = new PemReader(reader);
-            var keyObject = pemReader.ReadObject();
-
-            if (keyObject is AsymmetricKeyParameter keyParameter)
-                return keyParameter;
-            else if (keyObject is AsymmetricCipherKeyPair keyPair)
-                return keyPair.Private;
-
-            throw new InvalidOperationException("Invalid key format.");
         }
     }
 }
