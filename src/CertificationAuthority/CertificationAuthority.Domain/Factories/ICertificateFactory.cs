19d2253b46207b8c644b5f971cc0f307f3952942
Yuri Melo <yurifullstack@gmail.com>
2025-06-07 12:44:48 -0300

    Add new override method Factory with byte[] for Public Key

--- src/CertificationAuthority/CertificationAuthority.Domain/Factories/ICertificateFactory.cs
+++ src/CertificationAuthority/CertificationAuthority.Domain/Factories/ICertificateFactory.cs
@@ -5,6 +5,7 @@ namespace CertificationAuthority.Domain.Factories;
 
 public interface ICertificateFactory
 {
+    PKICertificate Factory(string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, byte[] publicKey, SignatureAlgorithmEnum signatureAlgorithm);
     PKICertificate Factory(string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, string publicKey, SignatureAlgorithmEnum signatureAlgorithm);
     PKICertificate Factory(Guid identifier, string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, string publicKey, SignatureAlgorithmEnum signatureAlgorithm);
 }
--- src/CertificationAuthority/CertificationAuthority.Domain/Factories/PKICertificateFactory.cs
+++ src/CertificationAuthority/CertificationAuthority.Domain/Factories/PKICertificateFactory.cs
@@ -5,7 +5,10 @@ namespace CertificationAuthority.Domain.Factories;
 
 public class PKICertificateFactory : ICertificateFactory
 {
-    public PKICertificate Factory(string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, string publicKey, SignatureAlgorithmEnum signatureAlgorithm) => 
+    public PKICertificate Factory(string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, byte[] publicKey, SignatureAlgorithmEnum signatureAlgorithm) =>
+        new PKICertificate(issuerDN, serialNumber, notBefore, notAfter, subjectDN, publicKey, signatureAlgorithm);
+
+    public PKICertificate Factory(string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, string publicKey, SignatureAlgorithmEnum signatureAlgorithm) =>
         new PKICertificate(issuerDN, serialNumber, notBefore, notAfter, subjectDN, publicKey, signatureAlgorithm);
 
     public PKICertificate Factory(Guid identifier, string issuerDN, string serialNumber, DateTime notBefore, DateTime notAfter, string subjectDN, string publicKey, SignatureAlgorithmEnum signatureAlgorithm) =>
